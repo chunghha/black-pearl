237a40528d0370e2552e89bd6268f3cefea6a34a
Chris Ha <chunghha@users.noreply.github.com>
2020-10-20 08:13:33 -0500

    feat(wip): colors for blackCardin

---
+++ local-insider-install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp -R ~/projects/vscode/black-pearl ~/.vscode-insiders/extensions/
+rm -rf ~/.vscode-insiders/extensions/black-pearl/.git/
--- local-install.sh
+++ local-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cp -R ~/projects/vscode/black-pearl ~/.vscode-insiders/extensions/
-rm -rf ~/.vscode-insiders/extensions/black-pearl/.git/
+cp -R ~/projects/vscode/black-pearl ~/.vscode/extensions/
+rm -rf ~/.vscode/extensions/black-pearl/.git/
